f61ea5da8275b2c618a7276939227c7a741b9ca8
shunyaendoh1215 <shunyaendoh.1215@gmail.com>
2020-08-25 21:32:32 +0900

    ♻️  Refactoring: 不必要なコメントを削除§

--- .zsh/2_oh-my-zsh(deprecated).zsh
+++ .zsh/2_oh-my-zsh(deprecated).zsh
@@ -1,23 +1,2 @@
-#TODO(shunya): zplugに移行
-# # oh-my-zsh
-# export ZSH="/Users/shunya/.oh-my-zsh"
-# source $ZSH/oh-my-zsh.sh
-
-# # git plugin for oh-my-zsh
-# plugins=(git ruby osx bundler brew rails emoji-clock brew gem vagrant)
-
-# # themes
-#   # bullet-train
-#   ZSH_THEME="bullet-train"
-#   BULLETTRAIN_PROMPT_ORDER=(
-#     time
-#     dir
-#     git
-#   )
-#   ZSH_THEME="agnoster"
-
-#   # pure prompt
-#   autoload -U promptinit; promptinit
-#   prompt pure
 
 echo '2   ____________________________\/\\\___________________________________'
