c8007ec3eb1bcedfe40556c93749a93cc90f2614
shunyaendoh1215 <shunyaendoh.1215@gmail.com>
2020-09-15 21:32:25 +0900

    🐛 BugFix: fix battery bug

--- .tmux/get_battery_tmux.sh
+++ .tmux/get_battery_tmux.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-
-  if battery_info=$(/usr/bin/pmset -g ps | awk '{ if (NR == 2) print $2 " " $3 }' | sed -e "s/;//g" -e "s/%//") ; then
+  # awk '{ if (NR == 2) print $2 " " $3から変更
+  if battery_info=$(/usr/bin/pmset -g ps | awk '{ if (NR == 2) print $3 " " $4 }' | sed -e "s/;//g" -e "s/%//") ; then
   battery_quantity=$(echo $battery_info | awk '{print $1}')
+  # echo $battery_quantity
   if [[ ! $battery_info =~ "discharging" ]]; then
     battery="#[bg=cyan,fg=black] ⚡ $battery_quantity% #[default]"
   elif (( $battery_quantity < 16 )); then
